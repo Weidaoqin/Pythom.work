7ace4f9b8a0668da265b793a23c12bbae0404851
Weidaoqin <43089456+Weidaoqin@users.noreply.github.com>
2018-09-11 13:26:15 +0800

    snake.py
    #e2.1DrawPython.py
    import turtle
    turtle.setup(650,350,200,200)
    turtle.penup()
    turtle.fd(-250)
    turtle.pendown()
    turtle.pensize(25)
    turtle.pencolor("green")
    turtle.pensize(25)
    turtle.seth(-40)
    for i in range(2):
    	turtle.circle(40,80)
    	turtle.circle(-40,80)
    
    	
    turtle.pensize(25)
    turtle.pencolor("black")
    turtle.seth(-40)
    for i in range(2):
    	turtle.circle(40,80)
    	turtle.circle(-40,80)
    
    turtle.pencolor("green")
    turtle.pensize(25)
    turtle.seth(-40)
    turtle.pencolor("red")
    turtle.circle(16,180)
    turtle.fd(40*2/3)
    turtle.pensize(25)
    turtle.seth(-40)
    turtle.pensize(25)
    turtle.pencolor("black")
    turtle.circle(16,180)
    turtle.fd(40*2/3)
    turtle.seth(-20)
    for i in range(2):
    	turtle.circle(40,-80)
    	turtle.circle(-40,-80)

---
+++ snake2.0.py
@@ -0,0 +1,38 @@
+#e2.1DrawPython.py
+import turtle
+turtle.setup(650,350,200,200)
+turtle.penup()
+turtle.fd(-250)
+turtle.pendown()
+turtle.pensize(25)
+turtle.pencolor("green")
+turtle.pensize(25)
+turtle.seth(-40)
+for i in range(2):
+	turtle.circle(40,80)
+	turtle.circle(-40,80)
+
+	
+turtle.pensize(25)
+turtle.pencolor("black")
+turtle.seth(-40)
+for i in range(2):
+	turtle.circle(40,80)
+	turtle.circle(-40,80)
+
+turtle.pencolor("green")
+turtle.pensize(25)
+turtle.seth(-40)
+turtle.pencolor("red")
+turtle.circle(16,180)
+turtle.fd(40*2/3)
+turtle.pensize(25)
+turtle.seth(-40)
+turtle.pensize(25)
+turtle.pencolor("black")
+turtle.circle(16,180)
+turtle.fd(40*2/3)
+turtle.seth(-20)
+for i in range(2):
+	turtle.circle(40,-80)
+	turtle.circle(-40,-80)
